0ccc44935937affd35c7eb808ff420986b3e1814
Yaarit <sternyaarit@gmail.com>
2022-08-16 15:43:10 +0300

    progressBar

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
 'use strict';
 
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+
+//3-pointers next buttons
+const threePointers = document.getElementById('three-pointers');
+const editBtns3 = Array.from(threePointers.getElementsByClassName('edit'));
+const nextBtns3 = Array.from(threePointers.getElementsByClassName('next'));
+const circles3 = threePointers.querySelectorAll('.circle');
+const nextBtnA = document.getElementById('btn-next-A');
+const editBtnA = document.getElementById('btn-edit-A');
+const nextBtnOB = document.getElementById('btn-next-OB');
+const editBtnOB = document.getElementById('btn-edit-OB');
+const nextBtnC3 = document.getElementById('btn-next-C3');
+const editBtnC3 = document.getElementById('btn-edit-C3');
+const nextBtnB = document.getElementById('btn-next-B');
+const editBtnB = document.getElementById('btn-edit-B');
+const formA = document.getElementById('formA');
+const moduleA = document.getElementById('module-a');
+
+//4-pointers next buttons
+const fourPointers = document.getElementById('four-pointers');
+const editBtns4 = Array.from(fourPointers.getElementsByClassName('edit'));
+const nextBtns4 = Array.from(fourPointers.getElementsByClassName('next'));
+const nextBtnC4 = document.getElementById('btn-next-C4');
+const editBtnC4 = document.getElementById('btn-edit-C4');
+const nextBtnCOBE4 = document.getElementById('btn-next-COBE4');
+const editBtnCOBE4 = document.getElementById('btn-edit-COBE4');
+const nextBtnE4 = document.getElementById('btn-next-E4');
+const editBtnE3 = document.getElementById('btn-edit-E4');
+const nextBtnD = document.getElementById('btn-next-D');
+const editBtnD = document.getElementById('btn-edit-D');
+const circles4 = fourPointers.querySelectorAll('.circle');
+
+//5-pointers next buttons
+const fivePointers = document.getElementById('five-pointers');
+const editBtns5 = Array.from(fivePointers.getElementsByClassName('edit'));
+const nextBtns5 = Array.from(fivePointers.getElementsByClassName('next'));
+const nextBtnE5 = document.getElementById('btn-next-E5');
+const editBtnE5 = document.getElementById('btn-edit-E5');
+const nextBtnCOBE5 = document.getElementById('btn-next-COBE5');
+const editBtnCOBE5 = document.getElementById('btn-edit-COBE5');
+const nextBtnG = document.getElementById('btn-next-G');
+const editBtnG = document.getElementById('btn-edit-G');
+const nextBtnF = document.getElementById('btn-next-F');
+const editBtnF = document.getElementById('btn-edit-F');
+const circles5 = fivePointers.querySelectorAll('.circle');
+
+let currentActive = 1;
+
 const hideClass = document.getElementsByClassName('hide');
 const points3 = document.querySelectorAll('.points3');
 const points4 = document.querySelectorAll('.points4');
@@ -125,15 +174,123 @@ function removeClass(pointsArr) {
 
 //To show the next step B
 function removeHideClass(el) {
-  setTimeout(function () {
-    el.classList.remove('hide');
-  }, 2000);
+  el.classList.remove('hide');
 }
 //To show the next step B
 function removeDisplayClass(el) {
-  setTimeout(function () {
-    el.classList.remove('step2');
-  }, 10000);
+  el.classList.remove('step2');
+}
+
+let start = 360;
+//To add event listener to the next button inside the grade box - 3 pointers
+for (let i = 0; i < nextBtns3.length; i++) {
+  nextBtns3[i].addEventListener('click', function () {
+    currentActive++;
+    if (currentActive > nextBtns3.length) {
+      currentActive = nextBtns3.length;
+      doneBtn3P.classList.remove('hide');
+    }
+    update3();
+    start = start - 90;
+    progress.style.background = `conic-gradient(#bdbdbd ${start}deg, #9c27b0 ${start}deg)`;
+    gradeBoxes[i].classList.remove('active'); //to close the gradebox
+  });
+}
+
+// //To add event listener to the edit button inside the grade box
+// for (let i = 0; i < editBtns3.length; i++) {
+//   editBtns3[i].addEventListener('click', function () {
+//     currentActive--;
+//     if (currentActive < 0) {
+//       currentActive = 0;
+//     }
+//     reloadForm();
+//   });
+// }
+
+// //Add active class to the circles to make them purple
+// function reloadForm() {
+//   circles3.forEach((circle, idx) => {
+//     if (idx < currentActive) {
+//       gradeBoxes[i].classList.add('active');
+//       formA.reset();
+//     } else {
+//       gradeBoxes[i].classList.remove('active');
+//     }
+//   });
+// }
+// // }
+
+//Add active class to the circles to make them purple
+function update3() {
+  circles3.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active-circle');
+    } else {
+      circle.classList.remove('active-circle');
+    }
+  });
+}
+
+start = 360;
+//To add event listener to the next button inside the grade box - 4 pointers
+for (let i = 0; i < nextBtns4.length; i++) {
+  nextBtns4[i].addEventListener('click', function () {
+    currentActive++;
+    if (currentActive > nextBtns4.length) {
+      currentActive = nextBtns4.length;
+      doneBtn4P.classList.remove('hide');
+    }
+    update4();
+    start = start - 90;
+    progress.style.background = `conic-gradient(#bdbdbd ${start}deg, #9c27b0 ${start}deg)`;
+    for (let i = 0; i < gradeBoxes.length; i++) {
+      gradeBoxes[i].classList.remove('active');
+    }
+  });
+}
+
+//To add event listener to the prev button inside the grade box
+for (let i = 0; i < editBtns4.length; i++) {}
+
+function update4() {
+  circles4.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active-circle');
+    } else {
+      circle.classList.remove('active-circle');
+    }
+  });
+}
+start = 360;
+//To add event listener to the next button inside the grade box - 5 pointers
+for (let i = 0; i < nextBtns5.length; i++) {
+  nextBtns5[i].addEventListener('click', function () {
+    currentActive++;
+    if (currentActive > nextBtns5.length) {
+      currentActive = nextBtns5.length;
+      doneBtn5P.classList.remove('hide');
+    }
+    update5();
+    start = start - 90;
+    progress.style.background = `conic-gradient(#bdbdbd ${start}deg, #9c27b0 ${start}deg)`;
+    for (let i = 0; i < gradeBoxes.length; i++) {
+      gradeBoxes[i].classList.remove('active');
+    }
+  });
+}
+
+//To add event listener to the prev button inside the grade box
+for (let i = 0; i < editBtns5.length; i++) {}
+
+function update5() {
+  circles5.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active-circle');
+    } else {
+      circle.classList.remove('active-circle');
+    }
+  });
 }
 
 //To display the final grade button after completing the input
@@ -187,7 +344,7 @@ for (let i = 0; i < btns.length; i++) {
   });
 }
 
-//When the user clicks the button, open the grade-box
+//When the user clicks on the circle, open the grade-box
 for (let i = 0; i < myBtns.length; i++) {
   myBtns[i].addEventListener('click', function () {
     if (currentGradeBox.length !== 0) {
@@ -200,7 +357,7 @@ for (let i = 0; i < myBtns.length; i++) {
   });
 }
 
-//When the user clicks on <span> (x), close the grade-box
+//When the user clicks on <span> (x) close the grade-box
 for (let i = 0; i < spans.length; i++) {
   spans[i].addEventListener('click', function () {
     for (let i = 0; i < gradeBoxes.length; i++) {
@@ -213,28 +370,34 @@ function calcGrade3070(hagasha, bagrut) {
   return hagasha * 0.3 + bagrut * 0.7;
 }
 
+/// Adding Step Progress Bar
+
 //5 Points
 
 //5 points module E calculator
 moduleECalculate.addEventListener('click', function () {
-  moduleEFinal.innerText = `Your Final Grade is ${calcGrade3070(
+  moduleEFinal.innerText = `Your Module E Grade is ${calcGrade3070(
     moduleEHagashaGrade.value,
     moduleEBagrutGrade.value
   )}`;
+  removeHideClass(nextBtnE5);
 });
+
 //5 points cobe calculator
 cobeCalculate.addEventListener('click', function () {
-  cobeFinal.innerText = `Your Final Grade is ${calcGrade3070(
+  cobeFinal.innerText = `Your COBE Grade is ${calcGrade3070(
     cobeHagashaGrade.value,
     cobeBagrutGrade.value
   )}`;
+  removeHideClass(nextBtnCOBE5);
 });
 //5 points module G calculator
 moduleGCalculate.addEventListener('click', function () {
-  moduleGFinal.innerText = `Your Final Grade is ${calcGrade3070(
+  moduleGFinal.innerText = `Your Module G Grade is ${calcGrade3070(
     moduleGHagashaGrade.value,
     moduleGBagrutGrade.value
   )}`;
+  removeHideClass(nextBtnG);
 });
 
 //To calculate the sum of elements in the array - and also to get the average
@@ -260,7 +423,7 @@ function calcAver(Arr) {
 // );
 
 moduleFCalculate.addEventListener('click', function () {
-  moduleFFinal.innerText = `Your Final Grade is ${
+  moduleFFinal.innerText = `Your Module F Grade is ${
     calcAver(
       literaryPieces.map(lp => {
         return +lp.value * 0.4;
@@ -277,6 +440,7 @@ moduleFCalculate.addEventListener('click', function () {
       })
     )
   }`;
+  removeHideClass(nextBtnF);
 });
 
 //final grade display 5 points
@@ -310,31 +474,34 @@ finalGradebtn5P.addEventListener('click', function () {
 //4 points
 //4 points module E calculator
 moduleECalculate4P.addEventListener('click', function () {
-  moduleEFinal4P.innerText = `Your Final Grade is ${calcGrade3070(
+  moduleEFinal4P.innerText = `Your Module E Grade is ${calcGrade3070(
     moduleEHagashaGrade4P.value,
     moduleEBagrutGrade4P.value
   )}`;
+  removeHideClass(nextBtnE4);
 });
 
 //4 points cobe calculator
 cobeCalculate4P.addEventListener('click', function () {
-  cobeFinal4P.innerText = `Your Final Grade is ${calcGrade3070(
+  cobeFinal4P.innerText = `Your COBE Grade is ${calcGrade3070(
     cobeHagashaGrade4P.value,
     cobeBagrutGrade4P.value
   )}`;
+  removeHideClass(nextBtnCOBE4);
 });
 
 //4 points module C calculator
 moduleCCalculate.addEventListener('click', function () {
-  moduleCFinal.innerText = `Your Final Grade is ${calcGrade3070(
+  moduleCFinal.innerText = `Your Module C Grade is ${calcGrade3070(
     moduleCHagashaGrade.value,
     moduleCBagrutGrade.value
   )}`;
+  removeHideClass(nextBtnC4);
 });
 
 //4 points module d calculator
 moduleDCalculate.addEventListener('click', function () {
-  moduleDFinal.innerText = `Your Final Grade is ${
+  moduleDFinal.innerText = `Your Module D Grade is ${
     calcAver(
       literaryPieces4P.map(lp => {
         return +lp.value * 0.4;
@@ -351,6 +518,7 @@ moduleDCalculate.addEventListener('click', function () {
       })
     )
   }`;
+  removeHideClass(nextBtnD);
 });
 
 //final grade display 4 points
@@ -385,31 +553,34 @@ finalGradebtn4P.addEventListener('click', function () {
 //3 points
 //3 points module A calculator
 moduleACalculate.addEventListener('click', function () {
-  moduleAFinal.innerText = `Your Final Grade is ${calcGrade3070(
+  moduleAFinal.innerText = `Your Module A Grade is ${calcGrade3070(
     moduleAHagashaGrade.value,
     moduleABagrutGrade.value
   )}`;
+  removeHideClass(nextBtnA);
 });
 
 //3 points oral bagrut calculator
 oralBagrutCalculate.addEventListener('click', function () {
-  oralBagrutFinal.innerText = `Your Final Grade is ${calcGrade3070(
+  oralBagrutFinal.innerText = `Your Oral Bagrut Grade is ${calcGrade3070(
     oralHagashaGrade.value,
     oralBagrutGrade.value
   )}`;
+  removeHideClass(nextBtnOB);
 });
 
 //3 points module C calculator
 moduleCCalculate3P.addEventListener('click', function () {
-  moduleCFinal3P.innerText = `Your Final Grade is ${calcGrade3070(
+  moduleCFinal3P.innerText = `Your Module C Grade is ${calcGrade3070(
     moduleCHagashaGrade3P.value,
     moduleCBagrutGrade3P.value
   )}`;
+  removeHideClass(nextBtnC3);
 });
 
 //3 points module b calculator
 moduleBCalculate.addEventListener('click', function () {
-  moduleBFinal.innerText = `Your Final Grade is ${
+  moduleBFinal.innerText = `Your Module B Grade is ${
     calcAver(
       literaryPieces3P.map(lp => {
         return +lp.value * 0.5;
@@ -426,6 +597,7 @@ moduleBCalculate.addEventListener('click', function () {
       })
     )
   }`;
+  removeHideClass(nextBtnB);
 });
 
 //final grade display 3 points
